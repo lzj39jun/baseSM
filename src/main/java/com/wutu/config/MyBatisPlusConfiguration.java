0d0aa22a6d9cd0278a479a2330aa6750b35d708b
liuzhenjun <>
2019-01-13 16:41:30 +0800

    分页

--- src/main/java/com/wutu/config/MyBatisPlusConfiguration.java
+++ src/main/java/com/wutu/config/MyBatisPlusConfiguration.java
@@ -2,6 +2,7 @@ package com.wutu.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +14,15 @@ public class MyBatisPlusConfiguration {
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
     }
 }
